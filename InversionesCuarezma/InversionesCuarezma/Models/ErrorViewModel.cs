464851b89d08381a325ab22141f87120798bb430
icuarezma <icuarezma@outlook.com>
2023-03-18 03:33:51 -0600

    file deletes

--- InversionesCuarezma/InversionesCuarezma/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace InversionesCuarezma.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
